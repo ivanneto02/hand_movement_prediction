6a693497f75336133d8ba994f381bf53802be489
ivanneto02 <72827265+ivanneto02@users.noreply.github.com>
2021-08-13 20:48:10 -0700

    added to .gitignore, added `tests` folder and files inside it.

---
+++ tests/image_input.py
@@ -0,0 +1,15 @@
+import cv2
+import mediapipe as mp
+
+def main():
+
+    mp_drawing = mp.solutions.drawing_utils
+    mp_hands = mp.solutions.hands
+    drawing_styles = mp.solutions.drawing_styles
+
+    IMAGE_FILES = ['img/']
+
+    pass
+
+if __name__ == '__main__':
+    main()
